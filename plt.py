fa92b2d9367502d69b08f94dd76f5f7c789312f3
jveloza <juan-veloza@hotmail.com>
2024-07-16 11:34:58 -0500

    Modifiqué mi gráfica

--- plt.py
+++ plt.py
@@ -3,9 +3,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x =  np.linspace(-5,5,100)
+x =  np.linspace(0,5,100)
 y = x**3
-plt.plot(x,y,"r--",label = "x^3", linewidth = 3)
+plt.plot(x,y,"r--",label = "$x^3$", linewidth = 5)
 plt.title("f(x) = x^3")
 plt.legend()
 plt.grid()
