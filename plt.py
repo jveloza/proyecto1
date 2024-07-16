8b488370deda1e77225b60d05626aa50b7822737
Juan David <juan-veloza@hotmail.com>
2024-07-16 10:29:42 -0500

    Se actualiza gráfica

--- plt.py
+++ plt.py
@@ -5,7 +5,7 @@ import numpy as np
 
 x =  np.linspace(-5,5,100)
 y = x**3
-plt.plot(x,y,"r--",label = "x^3")
+plt.plot(x,y,"r--",label = "x^3", linewidth = 3)
 plt.title("f(x) = x^3")
 plt.legend()
 plt.grid()
